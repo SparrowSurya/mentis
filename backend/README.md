579f90efbcf6d5870bc356cc0de925571d4fb3f8
sparrowsurya <thakur.suryakant.0406@gmail.com>
2024-09-07 23:47:35 +0530

    development environment setup with readme instructions

---
+++ backend/README.md
@@ -0,0 +1,10 @@
+# Mentis (Backend)
+
+# Setting Development environment
+1. Clone the [repo](https://github.com/sparrowsurya/mentis)
+2. Install requirements
+3. Create `.env` file:
+```
+SECRET_KEY=secret_key
+DEBUG=1
+```
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-zyj-v@yv7qau3uudx@2_21+ihzs*&$e2m&)=k2_2d(4&9du4bz"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", 0)
 
 ALLOWED_HOSTS = []
 
@@ -106,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Kolkata"
 
 USE_I18N = True
 
